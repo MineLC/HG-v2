44c726b006226ae8c87cac71c0576f02791cdc96
iChocoMilk <ichocodev@gmail.com>
2024-04-15 23:29:13 -0300

    Add teleport on send to spawn

--- src/main/java/lc/minelc/hg/others/spawn/SpawnStorage.java
+++ src/main/java/lc/minelc/hg/others/spawn/SpawnStorage.java
@@ -41,6 +41,7 @@ public final class SpawnStorage {
         inventory.setArmorContents(null);
         inventory.setItem(shopItem.slot(), shopItem.item());
         inventory.setItem(gameSelect.slot(), gameSelect.item());
+        player.teleport(location);
     }
 
     public boolean isInSpawn(final HumanEntity player) {
